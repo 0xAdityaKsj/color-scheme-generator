b602859f9c77183755cc801bcd668984a5979f64
Aditya <aditya.kantipudi@protonmail.com>
2023-07-04 20:56:18 +0530

    update, html, css, js

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-fetch("https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html&mode=analogic&count=6")
-    .then(res => res.json())
-    .then(data => console.log(data))
+const seedColorEl = document.getElementById('seed-color')
+const btnEl = document.getElementById('btn')
+const colorSchemeEl = document.getElementById('color-scheme')
+const mainEl = document.getElementById('main')
+
+
+
+
+
+btnEl.addEventListener("click", function () {
+    let colorPicked = seedColorEl.value.substring(1)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorPicked}&mode=${colorSchemeEl.value}&count=6`)
+        .then(res => res.json())
+        .then(data => {
+
+            console.log(data)
+            mainEl.innerHTML = `<div>
+                                    <img src=${data.colors[0].image.bare}>
+                                    <p>${data.colors[0].hex.value}</p>
+                                </div>
+                                <div>
+                                    <img src=${data.colors[1].image.bare}>
+                                    <p>${data.colors[1].hex.value}</p>
+                                </div>
+                                <div>
+                                    <img src=${data.colors[2].image.bare}>
+                                    <p>${data.colors[2].hex.value}</p>
+                                </div>
+                                <div>
+                                    <img src=${data.colors[3].image.bare}>
+                                    <p>${data.colors[3].hex.value}</p>
+                                </div>
+                                <div>
+                                    <img src=${data.colors[4].image.bare}>
+                                    <p>${data.colors[4].hex.value}</p>
+                                </div>
+                                <div>
+                                    <img src=${data.colors[5].image.bare}>
+                                    <p>${data.colors[5].hex.value}</p>
+                                </div>`
+        })
+})
+
+
